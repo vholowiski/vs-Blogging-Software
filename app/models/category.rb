d4d184a1cd1708e2fef71c00f283b0857327d2d4
dholowiski <david@holowiski.com>
2012-02-15 19:44:41 -0700

    categories and tags now sort properly too

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :categorizations
-	has_many :blogs, :through => :categorizations
+	has_many :blogs, :through => :categorizations, :order => 'created_at DESC'
 	
 	def to_param
 	  "#{id}-#{name.downcase.gsub(/[^a-zA-Z0-9]+/, '-').gsub(/-{2,}/, '-').gsub(/^-|-$/, '')}"
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
 	has_many :blog_tags
-	has_many :blogs, :through => :blog_tags
+	has_many :blogs, :through => :blog_tags, :order => 'created_at DESC'
 	
 	def to_param
 	  "#{id}-#{tag.downcase.gsub(/[^a-zA-Z0-9]+/, '-').gsub(/-{2,}/, '-').gsub(/^-|-$/, '')}"
